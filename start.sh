58734e5e3829e00929bdd993d5b4217de40a8557
rickyariesta <68098675+rickyariesta@users.noreply.github.com>
2020-07-16 16:33:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,32 @@ python bot.py +12242537182 zec
 python bot.py +12242537182 ltc
 python bot.py +12242537182 btc
 python bot.py +12242537182 bch
+python bot.py +6281245239922 doge
+python bot.py +6281245239922 zec
+python bot.py +6281245239922 ltc
+python bot.py +6281245239922 btc
+python bot.py +6281245239922 bch
+python bot.py +18722044299 doge
+python bot.py +18722044299 zec
+python bot.py +18722044299 ltc
+python bot.py +18722044299 btc
+python bot.py +18722044299 bch
+python bot.py +18722156370 doge
+python bot.py +18722156370 zec
+python bot.py +18722156370 ltc
+python bot.py +18722156370 btc
+python bot.py +18722156370 bch
+python bot.py +16614628317 doge
+python bot.py +16614628317 zec
+python bot.py +16614628317 ltc
+python bot.py +16614628317 btc
+python bot.py +16614628317 bch
+python bot.py +18722100435 doge
+python bot.py +18722100435 zec
+python bot.py +18722100435 ltc
+python bot.py +18722100435 btc
+python bot.py +18722100435 bch
+
 clear
 pwd
 bash start.sh
